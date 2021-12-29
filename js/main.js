a0f681a91777a4ec6010079cde1a6cdf54c29b66
omegaproger <49551143+omegaproger@users.noreply.github.com>
2021-12-29 23:34:26 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,73 @@
+
+const swiper = new Swiper('.buy__swiper', {
+effect: "coverflow",
+speed: 400,
+autoHeight: true,
+centerSlides:true,
+loop:false,
+resistance:false,
+slidesPerView:1,
+spaceBetween:10,
+centerInsufficientSlides:true,
+coverflowEffect: {
+  rotate: 1,
+  stretch: 0,
+  depth: 180,
+  modifier: 0,
+  slideShadows: false,
+},
+resizeObserver:false,
+setWrapperSize:true,
+
+breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: -220,
+      coverflowEffect: {
+			  modifier: 4,
+			},
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 1,
+      spaceBetween: -200,
+      coverflowEffect: {
+			  modifier: 4,
+			},
+    },
+    // when window width is >= 640px
+    1024: {
+
+      slidesPerView: 4,
+      spaceBetween: -30,
+      // slidesOffsetAfter:0,
+      // slidesOffsetBefore:0,
+      coverflowEffect: {
+			  modifier: 0,
+
+			},
+    }
+}
+
+});
+
+const winSwiper = new Swiper('.winners__swiper', {
+  speed: 400,
+  spaceBetween: 150,
+  effect: 'coverflow',
+  slidesPerView: 1,
+  // loop:true,
+  coverflowEffect: {
+    rotate: 1,
+    stretch: 0,
+    depth: 180,
+    modifier: 0,
+    slideShadows: false,
+  },
+
+});
+
 var intElemScrollTop = document.scrollTop;
 console.log(intElemScrollTop);
 var link = document.querySelectorAll(".pagination__item")
@@ -16,6 +86,70 @@ function bindClick(i) {
 }
 
 
+// var item = document.querySelectorAll(".cart")
+// console.log(item);
+// var slide = document.querySelectorAll(".swiper-slide")
+// console.log(slide);
+// // var slidActive = document.querySelector(".swiper-slide-active")
+// // console.log(slidActive);
+// for(var i = 0; i < slide.length; i++) {
+//        slide[i].querySelector(".cart").addEventListener("click", cartClick(i));
+// }
+
+// 	function cartClick(i) {
+// 	  return function() {
+	    	
+// 	    	for(var i = 0; i < slide.length; i++) {
+// 	       slide[i].querySelector(".cart").classList.remove("cart_active");
+// 			}
+// 		    	this.classList.toggle("cart_active");
+				 		
+// 	    	// if(this.classList.contains("cart_active") === false){
+// 	    	// 	this.classList.add("cart_active");
+// 	    	// }
+// 	    	// else  {
+// 	    	// 	this.classList.remove("cart_active");
+// 	    	// }	    	
+// 	  };
+// 	}
+
+// for(var i = 0; i < item.length; i++) {
+//        item[i].addEventListener("click", bindClick(i));
+// }
+// function bindClick(i) {
+//   return function() {
+//     	for(var i = 0; i < item.length; i++) {
+//        item[i].classList.remove("cart_active");
+// 		}
+//     	this.classList.toggle("cart_active");	
+
+//     	// if(this.classList.contains("cart_active") === false){
+//     	// 	this.classList.add("cart_active");
+//     	// }
+//     	// else  {
+//     	// 	this.classList.remove("cart_active");
+//     	// }
+    	
+
+//   };
+// }	
+
+  // import Swiper from 'swiper/bundle';
+
+ 
+  // import 'swiper/css/bundle';
+
+
+  // init Swiper:
+
+
+
+
+// Now you can use all slider methods like
+// const swiper = document.querySelector('.swiper').swiper;
+
+// // Now you can use all slider methods like
+// swiper.slideNext();
 
 
 // link.addEventListener("click",function(){
